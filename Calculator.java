0a63527bee0bd85b84c61139677324a7f4a18e4c
radevich-a <radevich-aa@yandex.ru>
2020-10-16 09:56:53 +0300

    add Main

---
+++ Calculator.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculator {
+    private JFrame mainFrame;
+
+    public Calculator() {
+        mainFrame = new JFrame();
+
+        mainFrame.setTitle("Calculator");
+        mainFrame.setBounds(100,100,300,300);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainFrame.setLayout(new BorderLayout());
+        JPanel top = new JPanel();
+        top.setLayout(new BorderLayout());
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new GridLayout(5, 3));
+
+        mainFrame.add(top, BorderLayout.NORTH);
+        mainFrame.add(bottom, BorderLayout.CENTER);
+
+        JTextField outField = new JTextField();
+        outField.setEditable(false);
+        top.add(outField, BorderLayout.CENTER);
+
+        ActionButtonListener abListener = new ActionButtonListener(outField);
+        for (int i = 0; i <= 9; i++) {
+            JButton digitBtn = new JButton(String.valueOf(i));
+            bottom.add(digitBtn);
+            digitBtn.addActionListener(abListener);
+        }
+
+        JButton plusBtn = new JButton("+");
+        plusBtn.addActionListener(abListener);
+        bottom.add(plusBtn);
+        JButton minusBtn = new JButton("-");
+        minusBtn.addActionListener(abListener);
+        bottom.add(minusBtn);
+        JButton cancelBtn = new JButton("C");
+        bottom.add(cancelBtn);
+        JButton submitBtn = new JButton("Submit");
+        bottom.add(submitBtn);
+
+        cancelBtn.addActionListener(abListener);
+        submitBtn.addActionListener(abListener);
+
+        mainFrame.setVisible(true);
+    }
+}
